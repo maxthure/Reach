ca8724fdff880fe1de7358c85c8b92c3b5f5a97d
Robert Richter <robert.richter@rrcomtech.com>
2022-05-02 16:42:02 +0200

    Markdown Editor added

--- web-frontend/src/pages/OnlineCollaboration.js
+++ web-frontend/src/pages/OnlineCollaboration.js
@@ -1,38 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Navigation from "../components/navigation";
 
 import "../assets/css/documentation.css";
 import "../assets/css/elements.css"
 import {Link, useParams} from "react-router-dom";
+import MdEditor from "for-editor";
 
 function OnlineCollaboration() {
 
-    let { projectId } = useParams();
+    const [ mdText, setMdText ] = useState("");
 
+    let {projectId} = useParams();
     let url = "/projects/project_" + projectId;
+
+    let handleChange = (newValue) => {
+        setMdText(newValue);
+    }
+
     return (
         <div>
             <header>
                 <Navigation/>
             </header>
-            <Link to={ url }>Back</Link>
+            <Link to={url}>Back</Link>
             <div className={"documentation-content"}>
                 <div className={"recent-changes"}>
                     <h2>Recent Changes:</h2>
                 </div>
                 <div className={"seperator"}/>
-                <div className={"documentation"}>
-                    <h1>Documentation</h1>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-                    et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-                    et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                <div id="documentation-body">
+                    <MdEditor
+                        value={mdText}
+                        language={"en"}
+                        onChange={(val) => handleChange(val)}
+                    />
                 </div>
             </div>
         </div>
     );
+
 }
 
 export default OnlineCollaboration;
